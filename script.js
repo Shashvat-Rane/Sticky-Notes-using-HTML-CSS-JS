703b2cad19fd975e5c350da6c75a00b24e879514
Shashvat-Rane <shashvatrane24@gmail.com>
2024-06-24 11:48:17 +0530

    Notes adding and moving

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let zindexcount =1;
+let NoteObjects = [];
+let ids = 1;
+
+
+
 // Make the DIV element draggable:
 dragElement(document.getElementById("drag"));
+// dragElement(document.getElementById("drag2"));
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -41,3 +48,75 @@ function dragElement(elmnt) {
         document.onmousemove = null;
     }
 }
+
+const createStickyNoteFull = (note)=>{
+    let divhai = document.createElement('div');
+    divhai.classList.add('open-note');
+    divhai.id = `drag${note.id}`;
+
+    divhai.innerHTML=`
+        <div class="menu hide">
+            <div style="display: flex;">
+                <div class="color1"></div>
+                <div class="color2"></div>
+                <div class="color3"></div>
+                <div class="color4"></div>
+                <div class="color5"></div>
+                <div class="color6"></div>
+                <div class="color7"></div>
+            </div>
+            <div class="nlbtn">
+                <img src="./icons/list.png">
+                <h6 class="nlbtntxt">Notes List</h6>
+            </div>
+            <div class="dltbtn">
+                <img src="./icons/delete.png">
+                <h6 class="dltbtntxt">Delete Node</h6>
+            </div>
+        </div>
+
+        <div id="drag${note.id}header" class="header-for-note">
+            <div class="plusiconon">
+                <img src="./icons/plusblack.png">
+            </div>
+            <div class="settingcrossiconon">
+                <div class="settingiconon">
+                    <img src="./icons/optionblack.png">
+                </div>
+                <div class="closeiconon">
+                    <img src="./icons/closeblack.png">
+                </div>
+            </div>
+        </div>
+        <div class="textcontainer">
+            <textarea spellcheck="false" id="" placeholder="Take a note..." class="textareaon"></textarea>
+        </div>
+    `
+
+    divhai.style.zIndex=note.z;
+
+    document.querySelector('#mainspace').append(divhai);
+
+    
+    dragElement(document.getElementById(`drag${note.id}`));
+
+    document.querySelector(".settingiconon").addEventListener('click',()=>{
+        document.querySelector(".menu").classList.toggle('hide');
+    })
+}
+
+
+
+
+document.querySelector('#drag .plusiconclick').addEventListener('click',()=>{
+    let note = {
+        id:ids,
+        txt:'',
+        z:zindexcount,
+    };
+    NoteObjects.push(note);
+    ids=ids+1;
+    zindexcount=zindexcount+1;
+    createStickyNoteFull(note);
+    // console.log(NoteObjects)
+})
